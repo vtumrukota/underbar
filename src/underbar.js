bdec2628a4578923948acd97e1c123063f8898d5
Vivek Tumrukota <Vivek@Viveks-MacBook-Air.local>
2015-02-24 00:45:04 -0800

    Added .map

--- src/underbar.js
+++ src/underbar.js
@@ -148,9 +148,19 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-    // map() is a useful primitive iteration function that works a lot
-    // like each(), but in addition to running the operation on all
-    // the members, it also maintains an array of results.
+    var adj = [];
+    if(Array.isArray(collection)){
+      for(var i=0; i<collection.length; i++){
+        adj[i] = iterator(collection[i]);
+      }
+    return adj;
+    }
+    else if(typeof(collection) === "object"){
+      for (var key in collection){
+        adj.push(iterator(collection[key]));
+      }
+      return adj;
+    }
   };
 
   /*
